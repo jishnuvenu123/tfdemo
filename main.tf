ff0e013f895242488a801a232b78f8424e59132c
jishnuvenu123 <jishnu.venugopal@tcs.com>
2024-11-13 16:29:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-/*resource "aws_vpc" "vpc1" {
+resource "aws_vpc" "vpc1" {
   cidr_block = "10.111.0.0/16"
   tags = {
-    Name = "vpc from cli"
+    Name = "user09cli"
   }
-}*/
-resource "aws_instance" "ec2vm09" {
+}
+/*resource "aws_instance" "ec2vm09" {
   ami = "ami-06b21ccaeff8cd686"
   instance_type = t2.micro
 tags = {
     Name = "user09"
 }
-}
+}*/
