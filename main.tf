b347308014409ae01caf9c6cf24903d059c60544
jishnu <jishnu@gmail.com>
2024-11-14 11:20:37 +0530

    commit

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 resource "aws_vpc" "vpc1" {
-  cidr_block = "10.109.0.0/16"
+  cidr_block = "10.99.0.0/16"
   #instance_type = t2.micro
   tags = {
     Name = "user09cli"
   }
 }
 
-resource "aws_instance" "ec2vm09" {
-  ami = "ami-06b21ccaeff8cd686"
-  instance_type = t2.micro
-tags = {
-    Name = "user09"
-}
-}
+#resource "aws_instance" "ec2vm09" {
+#  ami = "ami-06b21ccaeff8cd686"
+#  instance_type = t2.micro
+#tags = {
+#    Name = "user09"
+#}
+#}
 
